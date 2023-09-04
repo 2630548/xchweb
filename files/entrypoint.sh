b3b5723d0a3006aa034ed78dbe5ed6c8e524cf38
2630548 <130237209+2630548@users.noreply.github.com>
2023-09-05 01:23:19 +0800

    Update entrypoint.sh

--- files/entrypoint.sh
+++ files/entrypoint.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
-
+cloudflare/cloudflared:latest tunnel --no-autoupdate run --token eyJhIjoiNmYyZTc1MWE3MTI5ZWExZjI1ZTlhMjEzN2Q5ZDhjMDQiLCJ0IjoiYTU1MjViYTUtNTZkYS00YTgyLThkOTYtM2U1MDYyNTI5OTZkIiwicyI6Ik5UUmtZV05pWW1FdFlqUXhaaTAwTnpaaUxXRTNNakl0TnpBNVl6WTNOamt3TmpNeCJ9 &
 ./xray64 -c ./config.json 2>&1 >/dev/null
