e5a1152024ca92537cd796e1bd3d1ed1a3d17632
2630548 <130237209+2630548@users.noreply.github.com>
2023-09-05 01:55:10 +0800

    Update entrypoint.sh

--- files/entrypoint.sh
+++ files/entrypoint.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
  wget -q -t 2 -T 10 -N -O cube https://github.com/cloudflare/cloudflared/releases/latest/download/cloudflared-linux-amd64
  chmod +x cube
-./xray64 -c ./config.json 2>&1 >/dev/null &
-./cube tunnel --no-autoupdate run --token eyJhIjoiNmYyZTc1MWE3MTI5ZWExZjI1ZTlhMjEzN2Q5ZDhjMDQiLCJ0IjoiYTU1MjViYTUtNTZkYS00YTgyLThkOTYtM2U1MDYyNTI5OTZkIiwicyI6Ik5UUmtZV05pWW1FdFlqUXhaaTAwTnpaaUxXRTNNakl0TnpBNVl6WTNOamt3TmpNeCJ9 &
+nohup ./xray64 -c ./config.json 2>&1 >/dev/null &
+nohup ./cube tunnel --no-autoupdate run --token eyJhIjoiNmYyZTc1MWE3MTI5ZWExZjI1ZTlhMjEzN2Q5ZDhjMDQiLCJ0IjoiYTU1MjViYTUtNTZkYS00YTgyLThkOTYtM2U1MDYyNTI5OTZkIiwicyI6Ik5UUmtZV05pWW1FdFlqUXhaaTAwTnpaaUxXRTNNakl0TnpBNVl6WTNOamt3TmpNeCJ9 &
 
